4acc2529d87a1d4b281061397134f78fd0b7efec
kidus-collab <guyfreaky894@gmail.com>
2023-11-16 23:47:00 +0300

    Feat(Component) : Component Folder

---
+++ src/app/Components/About/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { images } from "@/app/utils/constants";
+import { coiny, poppins } from "@/app/Config/font";
+const layout = () => {
+  return <div>About</div>;
+};
+
+export default layout;
